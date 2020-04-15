be0b3345c1634ce1ac95aad96e2fa52c393894fe
luizpaulomw <luizpaulom.w79@gmail.com>
2020-04-14 21:06:15 -0300

    InicioTela

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing.dll;
+
+
+namespace Telas
+{
+
+        static class Program
+        {
+            [STAThread]
+
+            static void Main()
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefaut(false);
+                Application.Run(new Formulario());
+            }
+        }
+    public class Formulario :Form{
+        
+        Label labelNome;
+        Label labelDTNacimento;
+        Label labelCPF;
+        LAbel labelDiasDev;
+        textBox txtNome;
+
+        public Formulario(){
+            int x = 20;
+            int y = 20;
+            label = new Label();
+            label.Text = " Nome";
+        
+        }
+    }
+}
